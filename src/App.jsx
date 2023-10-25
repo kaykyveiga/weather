01ed19270901da20e7268b5f205b7b6435a68e7f
kaykyveiga <kaykyveiga46@gmail.com>
2023-10-25 15:23:52 -0300

    the base is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Location from './components/Location/Location'
+import SearchCity from './components/SearchCity/SearchCity'
 import Weather from './components/Weather/Weather'
 
 function App() {
  
   return (
    <div className="App">
+    <SearchCity/>
+    <Location/>
     <Weather/>
    </div>
   )
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
-import fetchWeatherData from "../API/WeatherApi";
+import fetchWeatherData from "../../API/WeatherApi";
 
 function Weather() {
   const [weatherData, setWeatherData] = useState(null);
+  const currentDate = new Date();
+  const hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
   useEffect(() => {
     fetchWeatherData()
       .then(data => {
         setWeatherData(data)
-        console.log(data)
+
       })
       .catch(error => console.error('Error fetching data:', error));
   }, [])
@@ -16,7 +24,22 @@ function Weather() {
     return <div>Loading...</div>;
   }
   return (
-    <div>Weather</div>
+    <div id="weather">
+      <div id="forecast">
+        <p>Weather</p>
+        <p>{hours}:{minutes}</p>
+        <h2>25°</h2>
+        <p>Nublado <span>Status nuvem</span></p>
+      </div>
+      <div id="more-details">
+        <span>Umidade</span>
+        <p>90%</p>
+        <span>Vento</span>
+        <p>15km</p>
+        <span>Temperatura</span>
+        <p>25°C</p>
+      </div>
+    </div>
   )
 }
 
